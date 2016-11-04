7fdff2fe638f85add8133611123dd12f45b4df8a
aphirak sang-ngenchai <apiruk_sang-ngenchai@hotmail.com>
2016-11-04 23:10:14 +0700

    setting mode finish and add buzzer

--- ozone-shoes/ozone-shoes.ino
+++ ozone-shoes/ozone-shoes.ino
@@ -1,8 +1,18 @@
+/*
+   Project  : Ozone shoes
+   Hardware : Arduino UNO, LED, Buzzer, Relay
+   Author   : Bavensky :3
+   E-Mail   : apiruk326@gmail.com
+   Date     : 04/11/2016 [dd/mm/yyyy]
+*/
+
+/*********   Include Library *********/
 #include <EEPROM.h>
 #include <Wire.h>
 #include <LiquidCrystal_I2C.h>
 #include<CountUpDownTimer.h>
 
+/*********   Initial Variable *********/
 LiquidCrystal_I2C lcd(0x3f, 16, 2);
 CountUpDownTimer T(DOWN);
 
@@ -12,12 +22,21 @@ CountUpDownTimer T(DOWN);
 #define LED_STOP 6
 #define RELAY_FAN 5
 #define RELAY_OZONE 4
+#define BUZZER 3
 #define db 200
 
+#define debounce 50
+#define holdTime 1000
+int btnLast = 0;
+boolean ignore = false;
+unsigned long btnDownT;
+unsigned long btnUpT;
+
 int address = 9;
-int timecount = 5;
+int timecount = 0;
 int mode = 0;
 
+/*********   Sub Function *********/
 void timeCount()  {
   digitalWrite(LED_COUNT, HIGH);
   digitalWrite(LED_STOP, LOW);
@@ -26,11 +45,20 @@ void timeCount()  {
   if (T.ShowMinutes() > 0)  {
     digitalWrite(RELAY_FAN, HIGH);
     digitalWrite(RELAY_OZONE, LOW);
+//    lcd.setCursor(0, 1);
+//    lcd.print("Ozone ");
   } else if (T.ShowMinutes() <= 0)  {
     digitalWrite(RELAY_FAN, LOW);
     digitalWrite(RELAY_OZONE, HIGH);
+//    lcd.setCursor(0, 1);
+//    lcd.print("Fan   ");
   }
-
+  
+  if(T.ShowMinutes() == 1)  {
+    toneBuzzer(50, 50);
+    toneBuzzer(50, 50);
+  }
+  
   if (digitalRead(B_START) == 0)  {
     delay(db);
     digitalWrite(RELAY_FAN, HIGH);
@@ -41,32 +69,85 @@ void timeCount()  {
   if (T.ShowMinutes() <= 0 && T.ShowSeconds() <= 0)  {
     digitalWrite(RELAY_FAN, HIGH);
     digitalWrite(RELAY_OZONE, HIGH);
+    digitalWrite(LED_COUNT, LOW);
+    digitalWrite(LED_STOP, HIGH);
     lcd.clear();
     lcd.setCursor(0, 0);
     lcd.print("   Ozone shoes  ");
     lcd.setCursor(0, 1);
-    lcd.print("    Time out    ");
-    delay(2000);
+    lcd.print("    Complete    ");
+    toneBuzzer(1000, 0);
+    toneBuzzer(100, 50);
+    toneBuzzer(100, 50);
     mode = 0;
   }
 
   lcd.setCursor(0, 0);
-  lcd.print("   Ozone shoes  ");
+    lcd.print("   Ozone shoes  ");
+  //  lcd.setCursor(6, 1);
   lcd.setCursor(0, 1);
-  lcd.print("count : ");
+  lcd.print("Count ");
   if (T.ShowMinutes() < 10)  lcd.print("0");
   lcd.print(T.ShowMinutes());
   lcd.print(":");
   if (T.ShowSeconds() < 10)  lcd.print("0");
   lcd.print(T.ShowSeconds());
-  lcd.print(" s   ");
+  lcd.print(" m/s   ");
 }
 
 void setTimes()  {
-  T.SetTimer(0, timecount, 0);
-  T.StartTimer();
+  if (digitalRead(B_START) == LOW)  {
+    delay(db);
+    toneBuzzer(50, 50);
+    timecount += 1;
+  }
+  if (digitalRead(B_SET) == LOW)  {
+    delay(db);
+    if (digitalRead(B_SET) == HIGH && ignore == false)  {
+      toneBuzzer(50, 50);
+      timecount -= 1;
+    }
+  }
+
+  if (timecount <= 2) timecount = 2;
+
+
+  if (digitalRead(B_SET) == LOW && btnLast == HIGH && (millis() - btnUpT) > long(debounce)) {
+    btnDownT = millis();
+  }
+  if (digitalRead(B_SET) == HIGH && btnLast == LOW && (millis() - btnDownT) > long(debounce)) {
+    ignore = false;
+    btnUpT = millis();
+  }
+  if (digitalRead(B_SET) == LOW && (millis() - btnDownT) > long(holdTime)) {
+    ignore = true;
+    btnDownT = millis();
+    EEPROM.write(address, timecount);
+    toneBuzzer(50, 50);
+    toneBuzzer(50, 50);
+    toneBuzzer(50, 50);
+    toneBuzzer(100, 0);
+    mode = 0;
+  }
+  btnLast = digitalRead(B_SET);
+
+  lcd.setCursor(0, 0);
+  lcd.print("   Ozone shoes  ");
+  lcd.setCursor(0, 1);
+  lcd.print("  Set ");
+  if (timecount < 10) lcd.print("0");
+  lcd.print(timecount);
+  lcd.print(":00 m/s    ");
 }
 
+void toneBuzzer(int up, int down) {
+  digitalWrite(BUZZER, HIGH);
+  delay(up);
+  digitalWrite(BUZZER, LOW);
+  delay(down);
+}
+
+/*********   Main Code *********/
 void setup()
 {
   Serial.begin(115200);
@@ -77,14 +158,15 @@ void setup()
   pinMode(LED_STOP, OUTPUT);
   pinMode(RELAY_FAN, OUTPUT);
   pinMode(RELAY_OZONE, OUTPUT);
+  pinMode(BUZZER, OUTPUT);
 
   digitalWrite(LED_COUNT, LOW);
   digitalWrite(LED_STOP, LOW);
   digitalWrite(RELAY_FAN, HIGH);
   digitalWrite(RELAY_OZONE, HIGH);
+  digitalWrite(BUZZER, LOW);
 
-  //   EEPROM.write(addr, val);
-  //  timecount = EEPROM.read(address);
+  timecount = EEPROM.read(address);
 
   lcd.begin();
   lcd.backlight();
@@ -92,11 +174,8 @@ void setup()
   lcd.print("     Welcome    ");
   lcd.setCursor(0, 1);
   lcd.print("   Ozone shoes  ");
-  delay(2000);
+  delay(500); // 5000
   lcd.clear();
-
-  T.SetTimer(0, timecount, 0);
-  T.StartTimer();
 }
 
 void loop()
@@ -108,20 +187,38 @@ void loop()
     lcd.setCursor(0, 0);
     lcd.print("   Ozone shoes  ");
     lcd.setCursor(0, 1);
-    lcd.print("Time count = ");
+    lcd.print("Time  ");
+    if (timecount < 10) lcd.print("0");
     lcd.print(timecount);
-    lcd.print(" s   ");
+    lcd.print(":00 m/s  ");
 
     if (digitalRead(B_START) == 0)  {
       delay(db);
+      toneBuzzer(1000, 0);
       T.SetTimer(0, timecount, 0);
+      T.StartTimer();
       mode = 1;
     }
 
-    Serial.print(digitalRead(B_START));
-    Serial.print("  ");
-    Serial.println(digitalRead(B_SET));
-    delay(100);
+    if (digitalRead(B_SET) == LOW && btnLast == HIGH && (millis() - btnUpT) > long(debounce)) {
+      btnDownT = millis();
+    }
+    if (digitalRead(B_SET) == HIGH && btnLast == LOW && (millis() - btnDownT) > long(debounce)) {
+      ignore = false;
+      btnUpT = millis();
+    }
+    if (digitalRead(B_SET) == LOW && (millis() - btnDownT) > long(holdTime)) {
+      ignore = true;
+      btnDownT = millis();
+      toneBuzzer(50, 50);
+      toneBuzzer(50, 50);
+      toneBuzzer(50, 50);
+      toneBuzzer(100, 0);
+      lcd.clear();
+      mode = 2;
+    }
+    btnLast = digitalRead(B_SET);
+
   }
   while (mode == 1) {
     timeCount();
